dd409fe214ab285614d3f60bb61c87fd221c50cf
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2020-04-11 11:06:48 +0300

    kirjautumisen perusversio valmis

--- WorkoutJournal/src/main/java/workoutjournal/UI/WorkoutJournalUI.java
+++ WorkoutJournal/src/main/java/workoutjournal/UI/WorkoutJournalUI.java
@@ -21,6 +21,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -42,7 +43,7 @@ public class WorkoutJournalUI extends Application {
         conn = DriverManager.getConnection("jdbc:sqlite:workoutjournal.db");
         Statement s = conn.createStatement();
         try {
-            s.execute("CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR NOT NULL, name VARCHAR NOT NULL, age INTEGER, sex INTEGER, maxHeartRate INTEGER)");
+            s.execute("CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR NOT NULL, password TEXT, maxHeartRate INTEGER)");
         } catch (SQLException ex) {
         }
         userDAO = new DBUserDAO(conn);
@@ -55,7 +56,7 @@ public class WorkoutJournalUI extends Application {
         
         GridPane loginPane = new GridPane();
         
-        loginPane.setPrefSize(600, 300);
+        loginPane.setPrefSize(720, 360);
         loginPane.setAlignment(Pos.CENTER);
         loginPane.setHgap(10);
         loginPane.setVgap(10);
@@ -68,6 +69,7 @@ public class WorkoutJournalUI extends Application {
         PasswordField passwordInput = new PasswordField();
         Button loginButton = new Button("Log in");
         Button newUserButton = new Button("Create new user");
+        Label loginError = new Label("");
         
         loginPane.add(loginInstruction, 0, 0);
         loginPane.add(usernameLabel, 0, 1);
@@ -76,6 +78,7 @@ public class WorkoutJournalUI extends Application {
         loginPane.add(passwordInput, 1, 2);
         loginPane.add(newUserButton, 0, 3);
         loginPane.add(loginButton, 1, 3);
+        loginPane.add(loginError, 0, 4);
         
         Scene loginScene = new Scene(loginPane);
         
@@ -83,30 +86,35 @@ public class WorkoutJournalUI extends Application {
         
         GridPane newUserPane = new GridPane();
         
-        newUserPane.setPrefSize(600, 300);
+        newUserPane.setPrefSize(720, 360);
         newUserPane.setAlignment(Pos.CENTER);
         newUserPane.setHgap(10);
         newUserPane.setVgap(10);
         newUserPane.setPadding(new Insets(10,10,10,10));
         
         Label newUserInstruction = new Label("Create new user");
-        Label setUserNameLabel = new Label("Username");
+        Label setUsernameLabel = new Label("Username");
         TextField setUsernameInput = new TextField();
-        Label setNameLabel = new Label("Name");
-        TextField setNameInput = new TextField();
+        Label setPasswordLabel = new Label("Password");
+        PasswordField setPasswordInput = new PasswordField();
         Label maxHeartRateLabel = new Label("Max heart rate");
         IntegerField maxHeartRateInput = new IntegerField();
         Label countMaxHeartRate = new Label("Count default max heart rate");
         Label age = new Label("Age");
         IntegerField ageInput = new IntegerField();
         Label sex = new Label("Sex");
-        ChoiceBox sexes = new ChoiceBox(FXCollections.observableArrayList("Female", "Male"));
+        ChoiceBox<String> sexes = new ChoiceBox();
+        sexes.getItems().addAll("female", "male");
+        sexes.getValue();
         Button countMaxHeartRateButton = new Button("Count max heart rate");
         Button createNewUserButton = new Button("Create new user");
+        Label userCreationError = new Label("");
         
         newUserPane.add(newUserInstruction, 0, 0);
-        newUserPane.add(setNameLabel, 0, 2);
-        newUserPane.add(setNameInput, 1, 2);
+        newUserPane.add(setUsernameLabel, 0, 1);
+        newUserPane.add(setUsernameInput, 1, 1);
+        newUserPane.add(setPasswordLabel, 0, 2);
+        newUserPane.add(setPasswordInput, 1, 2);
         newUserPane.add(maxHeartRateLabel, 0, 3);
         newUserPane.add(maxHeartRateInput, 1, 3);
         newUserPane.add(countMaxHeartRate, 0, 4);
@@ -116,22 +124,48 @@ public class WorkoutJournalUI extends Application {
         newUserPane.add(sexes, 1, 6);
         newUserPane.add(countMaxHeartRateButton, 1, 7);
         newUserPane.add(createNewUserButton, 1, 8);
+        newUserPane.add(userCreationError, 0, 9);
         
         Scene newUserScene = new Scene(newUserPane);
         
-        //Actions for buttons
+        // Primary scene
+        
+        GridPane primaryPane = new GridPane();
+        
+        primaryPane.add(new Label("Welcome!"), 0, 0);
+        
+        Scene primaryScene = new Scene(primaryPane);
+        
+        // Actions for buttons
         
         loginButton.setOnAction((event) -> {
             String username = usernameInput.getText();
             if (tools.login(username)) {
-                
+                stage.setScene(primaryScene);
             } else {
-                
+                loginError.setText("Invalid credentials.");
             }
         });
         
         newUserButton.setOnAction((event) -> {
             stage.setScene(newUserScene);
+        });
+        
+        countMaxHeartRateButton.setOnAction((event) -> {
+            maxHeartRateInput.setValue(tools.countMaxHeartRate(ageInput.getValue(), sexes.getValue()));
+        });
+        
+        
+        
+        createNewUserButton.setOnAction((event) -> {
+            if (tools.createUser(setUsernameInput.getText(), setPasswordInput.getText(), maxHeartRateInput.getValue())) {
+                loginInstruction.setText("New user created succesfully. You may now log in.");
+                stage.setScene(loginScene);
+            } else {
+                userCreationError.setText("Username is already in use. Please choose another username.");
+                setUsernameInput.clear();
+                setPasswordInput.clear();
+            }
         });
         
         stage.setScene(loginScene);
